f8a6409219ae7dc83d625cc1f0ef358a1168ab3a
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2026-09-23 14:52:55 +0800

    feat(completed): create checkbox and status update for tasks

--- lib/views/todo_widget.dart
+++ lib/views/todo_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todoapp/models/todo.dart';
+import 'package:todoapp/models/todo_list.dart';
 
 class TodoWidget extends StatefulWidget {
   const TodoWidget({Key? key, required this.todo}) : super(key: key);
@@ -10,19 +12,58 @@ final Todo todo;
 }
 
 class _TodoWidgetState extends State<TodoWidget> {
+  late bool _completed;
+
+  @override
+  void initState() {
+    super.initState();
+    _completed = widget.todo.completed;
+  }
+
+  @override
+  void didUpdateWidget(covariant TodoWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _completed = widget.todo.completed;
+  }
+
+  Future<void> _updateCompleted(bool? value) async {
+    if (value == null) return;
+
+    final updatedTodo = Todo(
+      id: widget.todo.id,
+      name: widget.todo.name,
+      description: widget.todo.description,
+      completed: value,
+    );
+
+    final model = Provider.of<TodoList>(context, listen: false);
+    await model.updateTodo(updatedTodo);
+
+    if (!mounted) return;
+
+    setState(() {
+      _completed = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-                  margin: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2,
-                    )
-                  ),
-                  padding: const EdgeInsets.all(2.5),
-                  child:  Text(widget.todo.name.toString(),
-                  textAlign: TextAlign.center),
-                );
+    return Card(
+      margin: const EdgeInsets.all(5),
+      child: CheckboxListTile(
+        title: Text(
+          widget.todo.name,
+          style: TextStyle(
+            decoration: _completed
+                ? TextDecoration.lineThrough
+                : TextDecoration.none,
+          ),
+        ),
+        subtitle: Text(widget.todo.description),
+        value: _completed,
+        onChanged: _updateCompleted,
+        controlAffinity: ListTileControlAffinity.leading,
+      ),
+    );
   }
 }
